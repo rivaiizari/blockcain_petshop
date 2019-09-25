9fc5477a1377ca90699780ba167d889e44d5c12f
rivaiizari <rivaiizari@gmail.com>
2019-09-25 20:57:49 +0700

    post cek out cripto v.1.1

--- truffle-config.js
+++ truffle-config.js
@@ -5,7 +5,7 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "0xB33D891A9e7C96058136B61C2110470eb6130d74" // Match any network id
+      network_id: "5777" // Match any network id
     },
     develop: {
       port: 8545
